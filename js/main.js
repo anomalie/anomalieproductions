7b63e4adece123ec717b94315f62cb3b73dc805d
Emilie Corpuz <emiliecorpuz@gmail.com>
2015-06-23 11:29:22 -0700

    update js comments

--- js/main.js
+++ js/main.js
@@ -1,51 +1,35 @@
-// (function(){
-//     angular
-//         .module('mainApp')
-//         .controller('mainController', mainController);
-    
-//     mainController.$inject = ['$scope'];
-    
-//     function mainController($scope){    
-        
-        // jQuery to collapse the navbar on scroll
-        $(window).scroll(function() {
-            if ($(".navbar").offset().top > 50) {
-                $(".navbar-fixed-top").addClass("top-nav-collapse");
-            } else {
-                $(".navbar-fixed-top").removeClass("top-nav-collapse");
-            }
-        });
+// Collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
 
-        // jQuery for page scrolling feature - requires jQuery Easing plugin
-        $(function() {
-            $('a.page-scroll').bind('click', function(event) {
-                var $anchor = $(this);
-                $('html, body').stop().animate({
-                    scrollTop: $($anchor.attr('href')).offset().top
-                }, 1500, 'easeInOutExpo');
-                event.preventDefault();
-            });
-        });
+// Page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
 
-        // Closes the Responsive Menu on Menu Item Click
-        $(function(){
-            $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
-            $('.navbar-toggle:visible').click();
-        });
-        });    
+// Closes the Responsive Menu on Menu Item Click
+$(function(){
+    $('.navbar-collapse ul li a:not(.dropdown-toggle)').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+});    
 
+// WOW Animated Scrolling Effects
 
-        wow = new WOW({
-                mobile: false
-            });
-            wow.init();
+wow = new WOW({
+        mobile: false
+    });
+    wow.init();
 
             
-
-
- 
-        
-
-// }
-
-// })();
